2e36cbe7ab83c958eb7b26dcee315a3494ad7b44
Someone <jannik-kocht@web.de>
2018-06-11 13:59:13 +0200

    fdfg

--- SuMAnwendung.java
+++ SuMAnwendung.java
@@ -9,7 +9,7 @@ import sum.komponenten.*;
 import sum.werkzeuge.*;
 import sum.ereignis.*;
 
-public class SuMAnwendung extends EBAnwendung
+public class SuMAnwendung extends EBAnwendung implements Runnable
 {
     // Objekte
     private Etikett hatEtikettDemokratur;
@@ -32,39 +32,103 @@ public class SuMAnwendung extends EBAnwendung
         super(1699, 980);
 
         hatEtikettDemokratur = new Etikett(256, 96, 100, 25, "Demokratur");
-        // Ausrichtung
         hatEtikettDemokratur.setzeAusrichtung(Ausrichtung.LINKS);
+        
         hatEtikettProzentsatzRote = new Etikett(150, 170, 100, 25, "Prozentsatz Rote");
-        // Ausrichtung
         hatEtikettProzentsatzRote.setzeAusrichtung(Ausrichtung.LINKS);
+        
         hatTextfeldRote = new Textfeld(250, 170, 100, 25, "");
-        // Ausrichtung
         hatTextfeldRote.setzeAusrichtung(Ausrichtung.LINKS);
+        
         hatKnopfUeberzeugen = new Knopf(150, 200, 100, 25, "Ueberzeugen");
         hatKnopfUeberzeugen.setzeBearbeiterGeklickt("hatKnopfUeberzeugenGeklickt");
+        
         hatKnopfBeenden = new Knopf(150, 230, 100, 25, "Beenden");
         hatKnopfBeenden.setzeBearbeiterGeklickt("hatKnopfBeendenGeklickt");
+        
         hatRechner = new Rechner();
         hatPerson = new Person[20][20];
+        
+        Thread t = new Thread(this);
+        t.start();
+
     }
 
-    /**
-    * Vorher: Ereignis GeklicktvonhatKnopfUeberzeugen fand statt.
-    * Nachher: Die Felder sind erzeugt
-    */
-    public void hatKnopfUeberzeugenGeklickt()
-    {
+    public void run() {
+        while(true) {
+            try {
+                this.bearbeiteLeerlauf();
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                
+            }
+        }
+    }
+    
+/**
+* Vorher: Ereignis GeklicktvonhatKnopfUeberzeugen fand statt.
+* Nachher: Die Felder sind erzeugt
+*/
+public void hatKnopfUeberzeugenGeklickt()
+{
        int lZufall;
        int lProzentRot = hatTextfeldRote.inhaltAlsGanzeZahl();
+       int l1H, l1V, l2H, l2V;
+       int lUeberzeuger;
        Buntstift stift = new Buntstift();
        for(int i = 0; i < 20; i++)
        for(int j = 0; j < 20; j++)
-       {lZufall = hatRechner.ganzeZufallszahl(1, 100);
-        hatPerson[i][j] = new Person(210 + i *  12, 40 + j * 12, lZufall <= lProzentRot, stift);
+       {
+           lZufall = hatRechner.ganzeZufallszahl(1, 100);
+           hatPerson[i][j] = new Person(210 + i *  12, 40 + j * 12, lZufall <= lProzentRot, stift);
        }
-       zSimuliert = true;
        
-    }
+       zSimuliert = true;
+       while(true) {
+           if (zSimuliert)
+           {
+                l1H = hatRechner.ganzeZufallszahl(0, 19);
+                l1V = hatRechner.ganzeZufallszahl(0, 19);
+                l2H = 0; l2V = 0;
+            
+                switch(hatRechner.ganzeZufallszahl(1,8))
+                {
+                    case 1: l2H = l1H -1; l2V = l1V - 1; 
+                    break;
+                    case 2: l2H = l1H; l2V = l1V-1;
+                    break;
+                    case 3: l2H = l1H + 1; l2V = l1V - 1;
+                    break;
+                    case 4: l2H = l1H - 1; l2V = l1V;
+                    break;
+                    case 5: l2H = l1H + 1; l2V = l1V;
+                    break;
+                    case 6: l2H = l1H - 1; l2V = l1V + 1;
+                    break;
+                    case 7: l2H = l1H; l2V = l1V + 1;
+                    break;
+                    case 8: l2H = l1H + 1; l2V = l1V +1;
+                    break;
+
+                }
+                if (l2H >= 0 && l2H < 20 && l2V>= 0 && l2V < 20)
+                {
+                    if(hatPerson[l1H][l1V].istRot() != hatPerson[l2H][l2V].istRot())
+                    {
+                        lUeberzeuger = hatRechner.ganzeZufallszahl(0, 1);
+                        if (lUeberzeuger == 0)
+                            hatPerson[l1H][l1V].setzeRot(hatPerson[l2H][l2V].istRot());
+                        else
+                            hatPerson[l2H][l2V].setzeRot(hatPerson[l1H][l1V].istRot());
+                    }
+                }
+            }
+           try {
+            Thread.sleep(1000);
+           } catch (InterruptedException e) {
+            }
+        }
+}
 
 /**
  * Vorher: Ereignis GeklicktvonhatKnopfBeenden fand statt.
@@ -116,5 +180,5 @@ public class SuMAnwendung extends EBAnwendung
                 }
             }
         }
-}
+} 
 }
